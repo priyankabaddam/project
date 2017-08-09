e569de230eb8c63b2ce91f3869a5c8d7eda99a14
Priyanka Baddam <pbaddam@opentext.com>
2017-08-09 17:00:26 +0530

    priyanka

---
+++ src/main/webapp/js/services.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+
+var services = angular.module('ngdemo.services', ['ngResource']);
+
+services.factory('DummyFactory', function ($resource) {
+    return $resource('/ngdemo/web/dummy', {}, {
+        query: { method: 'GET', params: {}, isArray: false }
+    })
+});
+
+services.factory('UsersFactory', function ($resource) {
+    return $resource('/ngdemo/web/users', {}, {
+        query: { method: 'GET', isArray: true },
+        create: { method: 'POST' }
+    })
+});
+
+services.factory('UserFactory', function ($resource) {
+    return $resource('/ngdemo/web/users/:id', {}, {
+        show: { method: 'GET' },
+        update: { method: 'PUT', params: {id: '@id'} },
+        delete: { method: 'DELETE', params: {id: '@id'} }
+    })
+});
